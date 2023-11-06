27ccdaa6724d23ae3235379d814c9a95358e8537
Rizal Miftakhul Rohman <rizal.miftakhul@dansmultipro.com>
2023-11-06 15:12:40 +0700

    refactor code category controller

--- src/controller/CategoryController.ts
+++ src/controller/CategoryController.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import type { Request,Response } from 'express';
-import { getAllCategory,getCategoryById,createCategory,updateCategory,deleteCategory } from "../service/categoryService";
+import * as categoryService from "../service/categoryService";
 import { createSuccesfull } from './BaseResponse';
 const router = express.Router();
 
 router.get("/",async (req:Request,res:Response) => {
     try{
-        const categories = await getAllCategory();
+        const categories = await categoryService.getAllCategory();
         const response = createSuccesfull("success","success get data",categories)
         res.status(200).send(response);
 
@@ -18,7 +18,7 @@ router.get("/",async (req:Request,res:Response) => {
 router.get("/:id",async(req:Request,res:Response)=>{
     try{
         const categoryId = req.params.id;
-        const category = await getCategoryById(categoryId);
+        const category = await categoryService.getCategoryById(categoryId);
         const response = createSuccesfull("success","success get data",category);
         res.status(200).send(response);
 
@@ -31,7 +31,7 @@ router.post("/create",async (req:Request,res:Response) => {
     try{
         const newCategory = req.body;
         console.log(newCategory);
-        const category = await createCategory(newCategory);
+        const category = await categoryService.createCategory(newCategory);
         const response = createSuccesfull("success","success create data",category)
         res.send(response)
     }catch(err : any){
@@ -43,7 +43,7 @@ router.put("/:id",async(req:Request,res:Response)=>{
    try{
     const categoryId = req.params.id;
     const categoryData = req.body;
-    const category = await updateCategory(categoryId,categoryData);
+    const category = await categoryService.updateCategory(categoryId,categoryData);
     const response = createSuccesfull("success","success update data",category);
     res.send(response);
    }catch(err : any){
@@ -53,7 +53,7 @@ router.put("/:id",async(req:Request,res:Response)=>{
 router.delete("/:id",async (req:Request,res:Response) => {
     try{
         const categoryId = req.params.id;
-        const category = await deleteCategory(categoryId);
+        const category = await categoryService.deleteCategory(categoryId);
         const response = createSuccesfull("success","success delete data", category);
         res.send(response);
     }catch(err : any){
